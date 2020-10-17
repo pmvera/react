2c94ebaeae622c92768f0c3eaf93cec560d44ba4
pmvera <pablomvera14@gmail.com>
2020-10-17 11:05:45 +0200

    jsx first version proyect

---
+++ jsx/src/index.js
@@ -0,0 +1,11 @@
+// Import the react and reactDom libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a react component
+const App = function() {
+    return (<div>Hi there!</div>)
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
